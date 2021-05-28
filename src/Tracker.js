0f6a5f1bd4957d136de5a26db93f573481b69e89
M. Fletcher <deadnancy@cpe-74-65-205-164.nyc.res.rr.com>
2021-05-28 09:42:07 -0400

    Simplify getting buses; include arrival info.

--- src/Tracker.js
+++ src/Tracker.js
@@ -8,9 +8,7 @@ import stops from './settings/stops'
 import { busTimeStopAPI, proxyURL } from './settings/busTimeAPI'
 import drawRoutes from './indicators/routes'
 import { drawBus, drawStops, drawUser } from './indicators/markers'
-import {
-  getBuses, getBusLine, getBusPosition, getTimestamp
-} from './destructurers/busTimeAPI'
+import { getBuses, getTimestamp } from './destructurers/busTimeAPI'
 import {
   mapAttr, tilesURL, mapCenter, mapZoom
 } from './settings/map'
@@ -30,30 +28,24 @@ function Tracker() {
       apiRequests.push(
         axios.post(proxyURL + encodeURIComponent(`${busTimeStopAPI}${stop.id}&nocache=${uniqid()}`))
           .then((response) => busStops.push({
+            data: response,
             id: stop.id,
             name: stop.name,
-            color: stop.color,
-            data: response
+            color: stop.color
           }))
       )
     ))
 
     Promise.all(apiRequests).then(() => {
       busStops.forEach((stop) => {
-        const timestamp = getTimestamp(stop.data)
-
-        const buses = getBuses(stop.data).map((bus) => (
-          { line: getBusLine(bus), position: getBusPosition(bus) }
-        ))
-
         setStopData((prevStopData) => ({
           ...prevStopData,
           [stop.id]: {
             id: stop.id,
             name: stop.name,
             color: stop.color,
-            buses,
-            timestamp
+            buses: getBuses(stop.data),
+            timestamp: getTimestamp(stop.data)
           }
         }))
       })
--- src/destructurers/busTimeAPI.js
+++ src/destructurers/busTimeAPI.js
@@ -1,17 +1,28 @@
 // https://bustime.mta.info/wiki/Developers/SIRIVehicleMonitoring
 
 const svcDelivery = (stop) => stop.data.contents.Siri.ServiceDelivery
-const getBuses = (stop) => svcDelivery(stop).StopMonitoringDelivery[0].MonitoredStopVisit
 const getTimestamp = (stop) => svcDelivery(stop).ResponseTimestamp.match(/\d\d:\d\d:\d\d/)
 
 const journey = (bus) => bus.MonitoredVehicleJourney
 const position = (bus) => journey(bus).VehicleLocation
+const call = (bus) => journey(bus).MonitoredCall
 const getBusLine = (bus) => journey(bus).PublishedLineName[0]
+
 const getBusPosition = (bus) => [
   [position(bus).Latitude, position(bus).Longitude],
   journey(bus).Bearing
 ]
 
-export {
-  getBuses, getBusLine, getBusPosition, getTimestamp
-}
+const getBusArrival = (bus) => [
+  call(bus).ArrivalProximityText,
+  call(bus).ExpectedArrivalTime
+]
+
+const getBuses = (stop) => svcDelivery(stop).StopMonitoringDelivery[0].MonitoredStopVisit
+  .map((bus) => ({
+    line: getBusLine(bus),
+    position: getBusPosition(bus),
+    arrival: getBusArrival(bus)
+  }))
+
+export { getBuses, getTimestamp }
